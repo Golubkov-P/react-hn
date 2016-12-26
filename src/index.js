354cc39552de0d3c10f2505beb1ab38add1a06f0
Golubkov Pavel <golubkov.p.d@gmail.com>
2016-12-26 12:30:04 +0500

    [Fix] delete unused module

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-import { match, Router, browserHistory } from 'react-router';
+import { Router, browserHistory } from 'react-router';
 
 import routes from './modules';
 
